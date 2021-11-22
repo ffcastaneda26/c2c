f489a84c4c5797fec3f607de86b42b6eb4f851ed
Federico Castañeda Ortiz <ffcastaneda@gmail.com>
2021-11-22 12:01:09 -0700

    Instalado Maatwebsite\Excel\Facades\Excel

--- config/app.php
+++ config/app.php
@@ -176,6 +176,7 @@ return [
         App\Providers\RouteServiceProvider::class,
         App\Providers\FortifyServiceProvider::class,
         App\Providers\JetstreamServiceProvider::class,
+        Maatwebsite\Excel\ExcelServiceProvider::class,
 
     ],
 
@@ -232,6 +233,7 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
 
+        'Excel' => Maatwebsite\Excel\Facades\Excel::class,
     ],
 
 ];
