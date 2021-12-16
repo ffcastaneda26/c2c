bc094c76cdd4c332414aa5cd0d693c7ef6c8fc06
Federico Castañeda Ortiz <ffcastaneda@gmail.com>
2021-12-16 16:52:39 -0700

    Controlador ftpController@ftpDownloadFIles lee archivos y actualiza inventario

--- app/Http/Controllers/FtpController.php
+++ app/Http/Controllers/FtpController.php
@@ -3,14 +3,20 @@
 namespace App\Http\Controllers;
 
 use Throwable;
-use Illuminate\Http\Request;
-use PhpParser\Node\Stmt\TryCatch;
+use App\Models\Inventory;
+
+use App\Imports\InventoryImport;
+
+use App\Models\TemporaryInventory;
+use Illuminate\Support\Facades\DB;
+use Maatwebsite\Excel\Facades\Excel;
 use App\Http\Controllers\ftpConnectionController;
 
 class FtpController extends Controller
 {
 
     public $ftpconnection;
+    public $files = ["dealermade_coast2coast.csv","dealermade_crossroads.csv"];
 
     public function __construct()
     {
@@ -22,14 +28,18 @@ class FtpController extends Controller
 
         if($this->ftpconnection->ftpLogin()){
             ftp_pasv($this->ftpconnection->ftp, TRUE);
-            $files = ["dealermade_coast2coast.csv","dealermade_crossroads.csv"];
+            TemporaryInventory::truncate();
             try {
-                foreach($files as $file){
+                foreach($this->files as $file){
                     if(!$this->get_file($file,$file)){
                         return "Ha habido error al intentar descar archivo $file";
                     }
+                    Excel::import(new InventoryImport,$file);
                 }
+                $this->copy_temporal_to_definitive_inventory();
+                TemporaryInventory::truncate();
             } catch (Throwable $e) {
+                dd($e);
                 return __('Some files could not have been downloaded');
             }
         }
@@ -37,6 +47,30 @@ class FtpController extends Controller
 
     private function get_file($source,$destination){
         return ftp_get( $this->ftpconnection->ftp, $destination, $source, FTP_BINARY);
-      }
+    }
+
+
+    private function copy_temporal_to_definitive_inventory(){
+        $locations = DB::table('temporary_inventories')->select('dealer_id')->distinct()->get()->toArray();
+        if(count($locations) == 2 ){
+            Inventory::truncate();
+            $temporary_inventory_records = TemporaryInventory::all()->toArray();
+            $this->create_inventory_record($temporary_inventory_records);
+        }else{
+            foreach($locations as $location ){
+                Inventory::where('dealer_id',$location->dealer_id)->delete();
+                $temporary_inventory_records = TemporaryInventory::where('dealer_id',$location->dealer_id)->get()->toArray();
+                $this->create_inventory_record($temporary_inventory_records);
+
+            }
+        }
+    }
+
+
+    private function create_inventory_record(array $temporary_inventory_records){
+        foreach($temporary_inventory_records as $temporary_inventory_record ){
+            Inventory::create($temporary_inventory_record);
+        }
+    }
 
 }
