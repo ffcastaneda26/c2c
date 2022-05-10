4e3765779d519701340a3b68ec10e16c9d78b27e
marcokodde <marco@koddeplus.com>
2022-05-10 16:54:35 -0500

    Mejora en vista de vehiculos

--- resources/views/inventory/real_list_vehicles.blade.php
+++ resources/views/inventory/real_list_vehicles.blade.php
@@ -1,7 +1,16 @@
+
 <div class="vehicle-listings">
     @foreach ($vehicles as $vehicle )
+    <script src="//dealermade.com/assets/media-layer/v2/dm.js"
+    data-dm-dealership-id="coast-to-coast-motors"
+    data-dm-insert-before-element-attribute="class"
+    data-dm-insert-before-element-attribute-value="vehicle-details"
+    data-dm-vehicle-vin="{{ $vehicle->vin }}">
+</script>
+<div class="vehicle-details"></div>
+
         <div class="vehicle">
-            <div class="listing-thumbnail">
+            <div class="gallery-block" style="width: 100%; float: left; margin-right: -100%; position: relative; opacity: 1; display: block; transition: opacity 0.5s ease 0s; z-index: 2;">
                 <a href="{{url('inventory/show/'. App::currentLocale() . '/' . $vehicle->id) }}">
                     @if(explode(",", $vehicle->images)[0])
                         <img  src="{{explode(",", $vehicle->images)[0] }}" onerror=this.src="{{ asset('images/default.jpeg') }}">
