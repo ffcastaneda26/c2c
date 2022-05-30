4d0bde1bf878bd62cb5c08525127f166ac58758f
Federico Castañeda Ortiz <ffcastaneda@gmail.com>
2022-05-30 15:59:45 -0600

    Cursor en plantilla de inventory.blade sobre los controles de avanzar o regresar

--- resources/views/livewire/inventory.blade.php
+++ resources/views/livewire/inventory.blade.php
@@ -1,18 +1,19 @@
 <div>
+
     <div class="slideshow-container" id="slide_image">
 
             @if($vehicle_show && $vehicle_show->images)
                 @foreach ( explode(",", $vehicle_show->images) as $image_url)
-                    @if( $loop->first or $loop->iteration  <= 3 )
+                    {{-- @if( $loop->first or $loop->iteration  <= 3 ) --}}
                         <div class="mySlides fade">
                             <img src="{{ $image_url }}"/>
                             <div class="text">{{$loop->iteration}}</div>
                         </div>
-                    @endif
+                    {{-- @endif --}}
                 @endforeach
             @endif
 
-        <a class="prev">&#10094;</a>
-        <a class="next">&#10095;</a>
+        <a class="prev cursor-pointer">&#10094;</a>
+        <a class="next cursor-pointer">&#10095;</a>
     </div>
 </div>
