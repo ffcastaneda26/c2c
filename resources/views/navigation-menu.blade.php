3c551b5d094626dba28b84478c711058f6155728
marcokodde <marco@koddeplus.com>
2022-05-30 10:23:47 -0500

    Agregando validacion de menu, solo a usuarios autenticados

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -1,3 +1,4 @@
+@auth
 <nav x-data="{ open: false }" class="bg-white border-b border-gray-100">
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
@@ -290,3 +291,5 @@
         </div>
     </div>
 </nav>
+@endauth
+
